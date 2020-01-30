97665188d3325770360b808be1eb2ffb73f18d8f
Oleg N <5213730-olnazx@users.noreply.gitlab.com>
2020-01-30 16:02:13 +0300

    Implement Redis Store

---
+++ src/stores/RedisStore.js
@@ -0,0 +1,68 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ * @private
+ */
+const { awaitTo: to } = require('../util');
+
+/**
+ * Local constants.
+ * @private
+ */
+const REDIS_CMD_NAME = 'increxpr';
+
+class RedisStore {
+  /**
+   * Constructor.
+   * @param {ioredis.Client} redis
+   */
+  constructor (redis) {
+    super();
+
+    // Define lua script for atomic increment and expire.
+    if (!redis[REDIS_CMD_NAME]) {
+      redis.defineCommand(
+        REDIS_CMD_NAME,
+
+        {
+          numberOfKeys: 2,
+
+          lua: `
+            local current = redis.call('incr', KEYS[1])
+
+            if tonumber(current) == 1 then
+              redis.call('expire', KEYS[1], KEYS[2])
+            end
+
+            return current
+          `
+        }
+      );
+    }
+
+    /**
+     * Redis Client (ioredis).
+     * @type {ioredis.Client}
+     */
+    this.redis = redis;
+  }
+
+  /**
+   * Increments value by key.
+   * @param {String} key Key to increment
+   * @param {Integer} ttl Key time-to-live, or time window (seconds)
+   * @returns {Integer} Current value
+   */
+  async increment (key, ttl) {
+    let [err, current] = await to(this.redis[REDIS_CMD_NAME](key, ttl));
+
+    if (err) {
+      throw err;
+    }
+
+    return current;
+  }
+}
+
+module.exports = RedisStore;
